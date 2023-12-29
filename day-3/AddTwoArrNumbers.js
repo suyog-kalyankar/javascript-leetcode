e0daec2444a16c9b7bb4cdf2de46d9d0042388c0
suyog kalyankar <suyog.kalyankar@gmail.com>
2023-12-29 10:15:04 +0100

    day-3

--- day-3/AddTwoArrNumbers.js
+++ day-3/AddTwoArrNumbers.js
@@ -5,14 +5,14 @@ const addTwoArrNumbers = (l1, l2) => {
     let quotient = 0;
     const stack = [];
     while(lenOfL1 >= 0 || lenOfl2 >= 0) {
-        let plus = 0;
+        let arrAddVal = 0;
         if (l1[lenOfL1]) {
-            plus += l1[lenOfL1]
+            arrAddVal += l1[lenOfL1]
         } 
         if (l2[lenOfl2]) {
-            plus += l2[lenOfl2]
+            arrAddVal += l2[lenOfl2]
         }
-        val = plus + quotient;
+        val = arrAddVal + quotient;
         quotient = Math.floor(val/10);
         let plusVal = (val%10);
         stack.push(plusVal);
